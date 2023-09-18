d915f3b63324e498bd2a79b556a4f0859e1f883a
tderleth <thomas.derleth@me.com>
2023-09-18 13:55:33 +0200

    added challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,12 @@
-function titleCase() {}
+function titleCase(sentence) {
+  if (typeof sentence !== "string") {
+    throw Error("not a string");
+  }
+
+  const words = sentence.split(" ");
+  const wordsAdjusted = words.map((word) => word[0].toUpperCase() + word.substring(1).toLowerCase());
+
+  return wordsAdjusted.join(" ");
+}
 
 module.exports = titleCase;
