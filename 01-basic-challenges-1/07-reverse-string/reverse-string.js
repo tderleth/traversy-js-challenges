993e022c2eb527777579830ff7d7594163ada328
tderleth <thomas.derleth@me.com>
2023-09-21 09:08:33 +0200

    reverse string task

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,12 @@
-function reverseString() {}
+function reverseString(stringToReverse) {
+  const arrayOfChars = stringToReverse.split("");
+
+  const result = [];
+  for (let index = arrayOfChars.length; index >= 0; index--) {
+    result.push(arrayOfChars[index]);
+  }
+
+  return result.join("");
+}
 
 module.exports = reverseString;
