50e5db071b94670faabab57c166037c598960799
Thomas Derleth <thomas.derleth@me.com>
2023-12-07 14:58:02 +0100

    added challenge

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,16 @@
-function countVowels() {}
+const VOWELS = ["a", "e", "i", "o", "u"];
+
+function countVowels(word) {
+  let count = 0;
+  const chars = word.toLowerCase().split("");
+
+  chars.forEach((char) => {
+    if (VOWELS.includes(char)) {
+      count++;
+    }
+  });
+
+  return count;
+}
 
 module.exports = countVowels;
