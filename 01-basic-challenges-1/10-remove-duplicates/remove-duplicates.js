46a31f0902037e98330f9dfc63a345c698c72d58
Thomas Derleth <thomas.derleth@me.com>
2023-12-08 11:25:17 +0100

    resolved daily challenge

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,21 @@
-function removeDuplicates() {}
+function removeDuplicates(duplicateArray) {
+  const result = [];
+
+  duplicateArray.forEach((element) => {
+    let isInArray = false;
+
+    result.forEach((resultElement) => {
+      if (element === resultElement) {
+        isInArray = true;
+      }
+    });
+
+    if (!isInArray) {
+      result.push(element);
+    }
+  });
+
+  return result;
+}
 
 module.exports = removeDuplicates;
