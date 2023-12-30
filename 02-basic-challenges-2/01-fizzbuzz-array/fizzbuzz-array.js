df263349bc9eca2721bceb2c5d12466f4e359ea9
Thomas Derleth <thomas.derleth@me.com>
2023-12-30 19:10:15 +0100

    fizzbuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const result = [];
+
+  for (let index = 1; index <= num; index++) {
+    if (index % 3 == 0 && index % 5 == 0) {
+      result.push("FizzBuzz");
+    } else if (index % 3 == 0) {
+      result.push("Fizz");
+    } else if (index % 5 == 0) {
+      result.push("Buzz");
+    } else {
+      result.push(index);
+    }
+  }
+
+  return result;
+}
 
 module.exports = fizzBuzzArray;
