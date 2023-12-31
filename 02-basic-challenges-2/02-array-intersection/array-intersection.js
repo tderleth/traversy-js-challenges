c4f45ab06c162d837f0532983f84c2773b78c761
Thomas Derleth <thomas.derleth@me.com>
2023-12-31 11:12:23 +0100

    intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(array1, array2) {
+  const result = [];
+
+  array1.forEach((element) => {
+    if (array2.includes(element)) {
+      result.push(element);
+    }
+  });
+
+  // return only unique ones
+  // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+  return [...new Set(result)];
+}
 
 module.exports = arrayIntersection;
