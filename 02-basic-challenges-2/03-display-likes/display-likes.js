c6cb5aeca76b398ced2f78a8cf962fea06de322f
Thomas Derleth <thomas.derleth@me.com>
2024-01-02 11:38:29 +0100

    display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,18 @@
-function displayLikes() {}
+function displayLikes(likes) {
+  const { length } = likes;
+
+  switch (length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${likes[0]} likes this`;
+    case 2:
+      return `${likes[0]} and ${likes[1]} like this`;
+    case 3:
+      return `${likes[0]}, ${likes[1]} and ${likes[2]} like this`;
+    default:
+      return `${likes[0]}, ${likes[1]} and ${length - 2} others like this`;
+  }
+}
 
 module.exports = displayLikes;
