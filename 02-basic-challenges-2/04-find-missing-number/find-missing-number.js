b22ae389c4e0ee8e68fa36d1ec4b80814b573b39
Thomas Derleth <thomas.derleth@me.com>
2024-01-03 10:39:31 +0100

    find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,19 @@
-function findMissingNumber() {}
+function findMissingNumber(array) {
+  let result;
+
+  array
+    .sort((a, b) => a - b)
+    .forEach((element, index) => {
+      if (index === array.length - 1) {
+        return;
+      }
+
+      if (array[index + 1] !== element + 1) {
+        result = element + 1;
+      }
+    });
+
+  return result;
+}
 
 module.exports = findMissingNumber;
