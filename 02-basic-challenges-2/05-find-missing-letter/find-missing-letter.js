12dc0c694aa35ab6ae0df32375abb192c618331a
Thomas Derleth <thomas.derleth@me.com>
2024-01-04 13:26:29 +0100

    find missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,13 @@
-function findMissingLetter() {}
+function findMissingLetter(lettersArray) {
+  const sortedLettersArray = lettersArray.sort();
+
+  for (let [index, value] of sortedLettersArray.entries()) {
+    const expectedNextLetter = String.fromCharCode(value.charCodeAt(0) + 1);
+
+    if (expectedNextLetter !== sortedLettersArray[index + 1]) {
+      return expectedNextLetter;
+    }
+  }
+}
 
 module.exports = findMissingLetter;
