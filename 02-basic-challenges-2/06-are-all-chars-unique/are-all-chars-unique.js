0117bbab7b66c76dbd9d982749033d361544651c
Thomas Derleth <thomas.derleth@me.com>
2024-01-05 10:45:06 +0100

    added isUnique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,18 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(inputString) {
+  let result = true;
+  const inputArray = inputString.split("");
+
+  for (let outerIndex = 0; outerIndex < inputArray.length && result; outerIndex++) {
+    const innerElement = inputArray[outerIndex];
+    for (let innerIndex = outerIndex + 1; innerIndex < inputArray.length && result; innerIndex++) {
+      const outerElement = inputArray[innerIndex];
+      if (innerElement === outerElement) {
+        result = false;
+      }
+    }
+  }
+
+  return result;
+}
 
 module.exports = areAllCharactersUnique;
