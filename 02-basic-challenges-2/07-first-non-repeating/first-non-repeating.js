ef0c00175a7aec65811bef6e23963434cda917e0
Thomas Derleth <thomas.derleth@me.com>
2024-01-08 10:19:32 +0100

    find first non repeating

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,15 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(repeatingString) {
+  let result = null;
+  const array = repeatingString.slice("");
+
+  for (let i = 0; i < array.length; i = i + 2) {
+    if (array[i] !== array[i + 1]) {
+      result = array[i];
+      break;
+    }
+  }
+
+  return result;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
