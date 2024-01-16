5189c7d681c03df564aa6449c2cdbe79d62196f8
Thomas Derleth <thomas.derleth@me.com>
2024-01-16 12:04:50 +0100

    email validation

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,8 @@
-function validateEmail() {}
+function validateEmail(email) {
+  // Create a regular expression to match the email format
+  const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  // Return whether the email matches the regular expression
+  return emailRegex.test(email);
+}
 
 module.exports = validateEmail;
