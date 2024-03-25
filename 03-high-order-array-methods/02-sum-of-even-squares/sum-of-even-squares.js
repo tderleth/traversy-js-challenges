ed04acfb0f0528265489ee842ab4b2178e8741af
Thomas Derleth <thomas.derleth@me.com>
2024-03-25 14:36:56 +0100

    Challenge: Sum of Squares of Even Numbers

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(array) {
+  return array
+    .filter((el) => el % 2 === 0)
+    .map((el) => el * el)
+    .reduce((total, number) => total + number, 0);
+}
 
 module.exports = sumOfEvenSquares;
