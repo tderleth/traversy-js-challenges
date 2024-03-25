dd94289cc76220fb4216eb6496ec9a0dac1d1bb6
Thomas Derleth <thomas.derleth@me.com>
2024-03-25 14:46:36 +0100

    Challenge: Calculate Total Sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,8 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const factor = 1 + taxRate / 100;
+  const amount = products.reduce((total, { quantity, price }) => total + quantity * price, 0);
+
+  return amount * factor;
+}
 
 module.exports = calculateTotalSalesWithTax;
