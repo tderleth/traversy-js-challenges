9b1077a4106f4323bf0e66c6810b64bc5c658842
Thomas Derleth <thomas.derleth@me.com>
2024-03-26 15:02:41 +0100

    Challenge: Highest Scoring Word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,17 @@
-function highestScoringWord() {}
+const alphabetMap = new Map([..."abcdefghijklmnopqrstuvwxyz"].map((letter, index) => [letter, index]));
+
+function highestScoringWord(sentence) {
+  let max = { word: "", count: 0 };
+
+  sentence.split(" ").forEach((word) => {
+    const count = [...word].reduce((total, char) => total + alphabetMap.get(char), 0);
+
+    if (count > max.count) {
+      max = { word, count };
+    }
+  });
+
+  return max.word;
+}
 
 module.exports = highestScoringWord;
