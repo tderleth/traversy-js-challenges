7e8038b7f4dc8b8631ae18fac99f58783871a100
Thomas Derleth <thomas.derleth@me.com>
2024-04-01 22:04:28 +0200

    hallenge: Valid Anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,17 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const array1 = str1.split("");
+  let array2 = str2.split("");
+
+  for (let char of array1) {
+    const index = array2.indexOf(char);
+    if (index !== -1) {
+      array2.splice(index, 1);
+    } else {
+      return false;
+    }
+  }
+
+  return array2.length === 0;
+}
 
 module.exports = validAnagrams;
