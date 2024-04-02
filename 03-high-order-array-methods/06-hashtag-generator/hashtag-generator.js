bfb4e60d416304e52891f99e90d0f9bfe18d9edf
Thomas Derleth <thomas.derleth@me.com>
2024-04-02 21:34:48 +0200

    Challenge: Hashtag Generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,15 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.length > 140 || str.trim() === "") {
+    return false;
+  }
+
+  return (
+    "#" +
+    str
+      .split(/\s+/)
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join("")
+  );
+}
 
 module.exports = generateHashtag;
