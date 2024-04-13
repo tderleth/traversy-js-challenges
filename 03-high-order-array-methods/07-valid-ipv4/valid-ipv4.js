dad5421c6205d09b95829609765653bf6ab31c8f
Thomas Derleth <thomas.derleth@me.com>
2024-04-13 12:03:21 +0200

    Challenge: Valid IPv4 Addresses

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,11 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ip) => {
+  const parts = ip.split(".");
+
+  if (parts.length !== 4) {
+    return false;
+  }
+
+  return parts.find((octet) => octet.startsWith("0") || Number(octet) > 255) ? false : true;
+};
 
 module.exports = isValidIPv4;
