7248a711d05ba572110bdd1a05ba64ecefeab2a5
Thomas Derleth <thomas.derleth@me.com>
2024-04-17 10:50:52 +0200

    Challenge: Car Mileage Analysis

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,15 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((prev, current) => prev + current.mileage, 0);
+  const averageMileage = totalMileage / cars.length;
+  const highestMileageCar = cars.reduce((prev, current) => (prev.mileage > current.mileage ? prev : current));
+  const lowestMileageCar = cars.reduce((prev, current) => (prev.mileage < current.mileage ? prev : current));
+
+  return {
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
