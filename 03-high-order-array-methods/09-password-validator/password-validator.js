9b87b4a5ce1278e644e67c87a3c8d780c7ad4740
Thomas Derleth <thomas.derleth@me.com>
2024-04-22 15:45:23 +0200

    Challenge: Password Validation

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,37 @@
-function validatePassword() {}
+function hasUpperCase(char) {
+  return char === char.toUpperCase();
+}
+
+function hasLowerCase(char) {
+  return char === char.toLowerCase();
+}
+
+function hasDigit(char) {
+  return char >= "0" && char <= "9";
+}
+
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false;
+  }
+
+  const chars = password.split("");
+
+  if (!chars.some(hasDigit)) {
+    return false;
+  }
+
+  const charsWithoutNumbers = password.replace(/[0-9]/g, "").split("");
+
+  if (!charsWithoutNumbers.some(hasUpperCase)) {
+    return false;
+  }
+
+  if (!charsWithoutNumbers.some(hasLowerCase)) {
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validatePassword;
