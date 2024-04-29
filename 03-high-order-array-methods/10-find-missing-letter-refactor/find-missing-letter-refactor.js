2bf605ad5934fd936464a6e68ae539a295787b21
Thomas Derleth <thomas.derleth@me.com>
2024-04-29 15:57:59 +0200

    Challenge: Find Missing Letter Refactor

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,19 @@
-function findMissingLetter() {}
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+function findMissingLetter(arr) {
+  let result = "";
+
+  for (const [index, element] of arr.entries()) {
+    const nextIndex = alphabet.findIndex((el) => el === element) + 1;
+    const nextElement = alphabet[nextIndex];
+
+    if (arr[index + 1] !== nextElement) {
+      result = nextElement;
+      break;
+    }
+  }
+
+  return result;
+}
 
 module.exports = findMissingLetter;
