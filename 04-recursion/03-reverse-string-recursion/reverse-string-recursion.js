685476cd6a0619754ec323bf9dc31ca66d450aa9
Thomas Derleth <thomas.derleth@me.com>
2024-05-07 16:15:29 +0200

    Challenge: Reverse String Recursion

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str.length === 1) return str;
+
+  const lastChar = str.substr(str.length - 1);
+  const stringWithoutLastChar = str.slice(0, -1);
+
+  return `${lastChar}${reverseString(stringWithoutLastChar)}`;
+}
 
 module.exports = reverseString;
