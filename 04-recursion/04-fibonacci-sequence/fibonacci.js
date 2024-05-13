51f6b6b2c30cf155b6895d48c9ca12382c73bd7e
Thomas Derleth <thomas.derleth@me.com>
2024-05-13 11:25:47 +0200

    Challenge: Fibonacci Sequence Using Recursion

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,13 @@
-function fibonacci() {}
+function fibonacci(num) {
+  if (num === 0) {
+    return 0;
+  }
+
+  if (num === 1) {
+    return 1;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
 
 module.exports = fibonacci;
