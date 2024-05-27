f474d7dd3488afe12aa92a0f99ac139f5ffbe170
Thomas Derleth <thomas.derleth@me.com>
2024-05-27 10:13:38 +0200

    Challenge: Array Sum Using Recursion

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,11 @@
-function arraySum() {}
+function arraySum(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  const first = arr.shift();
+
+  return first + arraySum(arr);
+}
 
 module.exports = arraySum;
