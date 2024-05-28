8242a1d739938a476dbdd42241ccfc73b9245fbc
Thomas Derleth <thomas.derleth@me.com>
2024-05-28 14:10:29 +0200

    Challenge: Number Range Using Recursion

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,11 @@
-function numberRange() {}
+function numberRange(start, end) {
+  if (start === end) {
+    return [end];
+  }
+
+  const numbers = numberRange(start, end - 1);
+
+  return numbers.push(end);
+}
 
 module.exports = numberRange;
