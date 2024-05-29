4e91e971051e2fbbf90efc628364cd208869ada1
Thomas Derleth <thomas.derleth@me.com>
2024-05-29 10:16:40 +0200

    Challenge: Flatten Array

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,11 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  const found = arr.find((element) => Array.isArray(element));
+
+  if (!found) {
+    return arr;
+  }
+
+  return flattenArray(arr.flatMap((el) => el));
+}
 
 module.exports = flattenArray;
